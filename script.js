e27764d4697dee7b00c67768da86dde62118a639
mamialex <mamialexboost@gmail.com>
2021-04-21 14:03:25 +0200

    second commit

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ let playerOneChoice = function (choice) {
 let playerTwoChoice = function (choice) {
   p2Choice = choice
   if (
-    (p1Choice === 'p' && p2Choice === "s") ||
-    (p1Choice === 'r' && p2Choice === 'p') ||
-    (p1Choice === 's' && p2Choice === 'r')
+    (p1Choice === 's' && p2Choice === 'p') ||
+    (p1Choice === 'p' && p2Choice === 'r') ||
+    (p1Choice === 'r' && p2Choice === 's')
   ) {
     console.log('P1 wins')
     document.getElementById("text-winner").innerText = "Player 1 wins"
   }
 
   if (
-    (p1Choice === 's' && p2Choice === 'p') ||
-    (p1Choice === 'p' && p2Choice === 'r') ||
-    (p1Choice === 'r' && p2Choice === 's')
+    (p1Choice === 'p' && p2Choice === 's') ||
+    (p1Choice === 'r' && p2Choice === 'p') ||
+    (p1Choice === 's' && p2Choice === 'r')
   ) {
     console.log('P2 wins')
     document.getElementById("text-winner").innerText = "Player 2 wins"
